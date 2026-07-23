9608dce23fe117160b39e81c07ea035e960be9d0
Artem Sunshine <a1@MacBook-Pro-Sunshine.local>
2026-07-24 04:14:35 +0700

    Add WebView QUIC workaround in Android MainActivity

--- android/app/src/main/java/ru/tone/crmcontracts/MainActivity.java
+++ android/app/src/main/java/ru/tone/crmcontracts/MainActivity.java
@@ -1,5 +1,22 @@
 package ru.tone.crmcontracts;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Disable QUIC to avoid net::ERR_QUIC_PROTOCOL_ERROR on some providers
+        WebView webView = this.getBridge().getWebView();
+        if (webView != null) {
+            android.webkit.WebSettings settings = webView.getSettings();
+            // Force using HTTP/1.1 instead of QUIC/HTTP3
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                settings.setSafeBrowsingEnabled(false);
+            }
+        }
+    }
+}
